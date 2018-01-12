939c47ef5e9db35da8aa06b3593df978a288a790
Matthew Taggart <matthew.taggart@cardcash.com>
2018-01-12 17:49:59 -0500

    Update webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -2,67 +2,81 @@ const { resolve } = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const cssDevMode = [ 'style-loader', 'css-loader', 'sass-loader' ];
+
+const cssDevMode = ['style-loader', 'css-loader', 'sass-loader'];
 const cssProdMode = ExtractTextPlugin.extract({
   fallback: 'style-loader',
-  use: [ 'css-loader', 'sass-loader' ]
+  use: ['css-loader', 'sass-loader'],
 });
 
 module.exports = ({ production = false }) => ({
   context: resolve('src'),
-  entry: [ './index.js', './style.scss' ],
+  entry: ['./index.jsx', './style.scss'],
   output: {
     path: resolve('dist'),
     filename: 'bundle.js',
-    pathinfo: !production
+    pathinfo: !production,
+    publicPath: '/',
   },
   devtool: production ? 'source-map' : 'eval',
   devServer: {
     contentBase: resolve('dist'),
     compress: true,
-    port: 9000
+    historyApiFallback: true,
+    port: 9000,
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: { presets: [ 'env', 'react' ] },
-        }
+          options: {
+            presets: [
+              ['env', {
+                targets: {
+                  node: 'current',
+                },
+              }],
+              'react',
+              'stage-2',
+            ],
+          },
+        },
       },
       {
         test: /\.scss$/,
         exclude: /node_modules/,
-        use: production ? cssProdMode : cssDevMode
-      }
-    ]
+        use: production ? cssProdMode : cssDevMode,
+      },
+    ],
   },
   plugins: [
     new ExtractTextPlugin({
       filename: './style.css',
       allChunks: true,
-      disable: !production
+      disable: !production,
     }),
     new HtmlWebpackPlugin({
       title: 'Project',
       minify: {
-        collapseWhitespace: production
+        collapseWhitespace: production,
       },
       hash: true,
-      template: './index.ejs'
+      template: './index.ejs',
     }),
     new webpack.optimize.UglifyJsPlugin({
       exclude: /node_modules/,
       compress: {
         warnings: false,
         drop_console: false,
-      }
+      },
+      sourceMap: true,
     }),
-    new webpack.NamedModulesPlugin()
+    new webpack.NamedModulesPlugin(),
   ],
   resolve: {
-    extensions: [ '.js', '.jsx' ]
-  }
+    extensions: ['.js', '.jsx'],
+  },
 });
